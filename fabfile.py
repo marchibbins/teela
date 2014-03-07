0e8e0b89c4bc5b00dac81c9355f4239a738a79e4
Marc Hibbins <marchibbins@gmail.com>
2014-03-07 17:03:30 +0000

    Install Gunicorn with fab command

--- fabfile.py
+++ fabfile.py
@@ -3,3 +3,7 @@ from fabric.api import *
 def run():
     """ Run with Flask server. """
     local('python main.py')
+
+def gunicorn():
+    """ Run with Gunicorn server. """
+    local('gunicorn -b 127.0.0.1:5000 main:app')

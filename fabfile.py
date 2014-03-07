23f0d495a6b31abadbd0d03734ed07b5421a7357
Marc Hibbins <marchibbins@gmail.com>
2014-03-07 17:33:23 +0000

    Simple shell file and fab command

--- fabfile.py
+++ fabfile.py
@@ -7,3 +7,7 @@ def run():
 def gunicorn():
     """ Run with Gunicorn server. """
     local('gunicorn -b 127.0.0.1:5000 main:app')
+
+def shell():
+    """ Start an interactive shell within application environment. """
+    local('python shell.py')
---
+++ shell.py
@@ -0,0 +1,8 @@
+import os
+import readline
+from pprint import pprint
+
+from flask import *
+from teela import *
+
+os.environ['PYTHONINSPECT'] = 'True'

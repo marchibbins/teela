c3b83bcaa8d2264695301fcb58ffdce6341541eb
Marc Hibbins <marchibbins@gmail.com>
2014-03-07 18:10:56 +0000

    Instantiate app with host and port

--- fabfile.py
+++ fabfile.py
@@ -6,7 +6,7 @@ def run():
 
 def gunicorn():
     """ Run with Gunicorn server. """
-    local('gunicorn -b 127.0.0.1:5000 main:app')
+    local('gunicorn main:app')
 
 def shell():
     """ Start an interactive shell within application environment. """
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = init()
 
 def run():
     """ Run locally. """
-    app.run()
+    app.run(host='0.0.0.0', port=5000)
 
 if __name__ == "__main__":
     run()

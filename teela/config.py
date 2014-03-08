2a4a5e8905b3a54155a012090430c2f4a191ca86
Marc Hibbins <marchibbins@gmail.com>
2014-03-08 20:12:03 +0000

    Remove admin email requirement

--- teela/config.py
+++ teela/config.py
@@ -25,8 +25,6 @@ class Config(object):
     DEBUG = env_var('DEBUG', default=False)
     SERVER_NAME = env_var('SERVER_NAME', required=True)
 
-    ADMIN = env_var('ADMIN', required=True)
-
     # Flask-cache: http://pythonhosted.org/Flask-Cache/
     CACHE_TYPE = env_var('CACHE_TYPE', default='simple')
     CACHE_DEFAULT_TIMEOUT = env_var('CACHE_DEFAULT_TIMEOUT', default=60)

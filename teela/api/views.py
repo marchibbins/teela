50ab403165307d275c179b0577d7021386a96be6
Marc Hibbins <marchibbins@gmail.com>
2014-03-08 13:37:33 +0000

    Add example custom 404 handler for blueprint

--- teela/api/views.py
+++ teela/api/views.py
@@ -7,3 +7,11 @@ api = Blueprint('api', __name__, url_prefix="/api")
 def index():
     """ Create a JSON response. """
     return jsonify(status='success', message='Hello world')
+
+
+@api.errorhandler(404)
+def page_not_found(error):
+    """ Custom 404 for this blueprint. Note: only handles exceptions
+        raised by view functions, not missing routes under this url prefix. """
+    # http://flask.pocoo.org/docs/api/#flask.Blueprint.errorhandler
+    return jsonify(status='error', message='Not found'), 404

54f4a99de420274a22fb99819781d9af13aea0dc
Marc Hibbins <marchibbins@gmail.com>
2014-03-12 21:40:58 +0000

    Set SQL config in environment

--- teela/config.py
+++ teela/config.py
@@ -27,8 +27,10 @@ class Config(object):
     DEBUG = env_var('DEBUG', default=False)
     SERVER_NAME = env_var('SERVER_NAME', required=True)
 
-    # Flask-cache: http://pythonhosted.org/Flask-Cache/
+    # Flask-Cache: http://pythonhosted.org/Flask-Cache/
     CACHE_TYPE = env_var('CACHE_TYPE', default='simple')
     CACHE_DEFAULT_TIMEOUT = env_var('CACHE_DEFAULT_TIMEOUT', default=60)
 
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///%s/db.sqlite' % PROJECT_ROOT
+    # Flask-SQLAlchemy: http://packages.python.org/Flask-SQLAlchemy/config.html
+    SQLALCHEMY_DATABASE_URI = env_var('SQLALCHEMY_DATABASE_URI', required=True)
+    SQLALCHEMY_ECHO = env_var('SQLALCHEMY_ECHO', default=False)

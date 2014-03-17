f4b421cc7b7d95c0b2359b37afde7822b6274465
Marc Hibbins <marchibbins@gmail.com>
2014-03-17 21:48:01 +0000

    Simple CRUD admin views for messages

--- teela/admin/views.py
+++ teela/admin/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, flash, render_template
+from flask import Blueprint, flash, render_template, redirect, request, url_for
 from flask.ext.login import login_required
 
 from teela.example.forms import MessageForm
@@ -17,12 +17,30 @@ def index():
     return render_template('admin/index.html', messages=messages)
 
 
+@admin.route('/message/add', methods=['GET', 'POST'])
+@login_required
+def message_add():
+    """ Create a new Message. """
+    form = MessageForm()
+    title = "Add message"
+
+    if form.validate_on_submit():
+        message = Message(text=form.text.data)
+        db.session.add(message)
+        db.session.commit()
+        flash('Message created.', 'success')
+        return redirect(url_for('admin.index'))
+
+    return render_template('admin/message/add_edit.html', form=form, title=title)
+
+
 @admin.route('/message/<int:message_id>', methods=['GET', 'POST'])
 @login_required
-def message(message_id):
+def message_edit(message_id):
     """ Edit a Message. """
     message = Message.query.filter_by(id=message_id).first_or_404()
     form = MessageForm(obj=message)
+    title = "Edit message"
 
     if form.validate_on_submit():
         form.populate_obj(message)
@@ -30,4 +48,19 @@ def message(message_id):
         db.session.commit()
         flash('Message updated.', 'success')
 
-    return render_template('admin/message.html', message=message, form=form)
+    return render_template('admin/message/add_edit.html', message=message, form=form, title=title)
+
+
+@admin.route('/message/<int:message_id>/delete', methods=['GET', 'POST'])
+@login_required
+def message_delete(message_id):
+    """ Delete a message. """
+    message = Message.query.filter_by(id=message_id).first_or_404()
+
+    if request.method == 'POST':
+        db.session.delete(message)
+        db.session.commit()
+        flash('Message deleted.', 'success')
+        return redirect(url_for('admin.index'))
+
+    return render_template('admin/message/delete.html', message=message)

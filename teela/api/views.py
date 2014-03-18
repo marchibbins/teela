fee02d7f18d45428a1372aa318a199bf98a49775
Marc Hibbins <marchibbins@gmail.com>
2014-03-18 23:22:43 +0000

    Add caching example to view

--- teela/api/views.py
+++ teela/api/views.py
@@ -1,13 +1,17 @@
 from flask import Blueprint, render_template, jsonify
+from datetime import datetime
+
+from teela.extensions import cache
 
 
 api = Blueprint('api', __name__, url_prefix='/api')
 
 
 @api.route('/')
+@cache.cached(timeout=60)
 def index():
     """ Create a JSON response. """
-    return jsonify(status='success', message='Hello world')
+    return jsonify(status='success', time=datetime.now())
 
 
 @api.errorhandler(404)
@@ -15,4 +19,4 @@ def page_not_found(error):
     """ Custom 404 for this blueprint. Note: only handles exceptions
         raised by view functions, not missing routes under this url prefix. """
     # http://flask.pocoo.org/docs/api/#flask.Blueprint.errorhandler
-    return jsonify(status='error', message='Not found'), 404
+    return jsonify(status='error', description='Not found'), 404
